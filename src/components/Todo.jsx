c586a0fe87410701f563482864683dd78280252d
sardorbeksidikov <sardorsidikov005@gmail.com>
2024-04-16 12:50:27 +0500

    sardorbek_todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,8 @@
-
+// Todo.js
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import TodoList from './TodoList';
+import FilterButtons from './FilterButtons';
 import { BsSearch, BsPlus } from 'react-icons/bs';
 import { addTodo, updateSearchTerm } from '../redux/actions';
 
@@ -49,10 +50,10 @@ const Todo = () => {
       </div>
 
       <div className="flex flex-col sm:flex-row items-center justify-between gap-4">
-       
+        <FilterButtons />
         <div className="flex items-center mb-4">
           <input
-            className="w-auto flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
+            className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
             type="text"
             placeholder="Search Todos"
             value={searchTerm}
